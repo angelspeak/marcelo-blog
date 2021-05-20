08ef5502b3d2f429ebe4956e8ff3b7fee040d630
Marcelo Lombardi <pico.del.angel@gmail.com>
2021-05-20 20:51:10 -0300

    Chages for Heroku

--- main.py
+++ main.py
@@ -229,9 +229,11 @@ def delete_post(post_id):
     return redirect(url_for('get_all_posts'))
 
 
-
-
+# if __name__ == "__main__":
+#     # app.run(host='127.0.0.1', port=5000)
+#     app.run(debug=True)
 
 if __name__ == "__main__":
-    # app.run(host='127.0.0.1', port=5000)
-    app.run(debug=True)
+    # For HEROKU
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
