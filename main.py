3fe6d3868720f99e5af5cd115995f173cb63f698
Marcelo Lombardi <pico.del.angel@gmail.com>
2021-05-20 20:33:34 -0300

    Initial Commit

--- main.py
+++ main.py
@@ -10,9 +10,10 @@ from forms import CreatePostForm, RegisterForm, LoginForm, CommentForm
 from flask_gravatar import Gravatar
 from flask_login import UserMixin, login_user, LoginManager, login_required, current_user, logout_user
 import flask
+import os
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
+app.config['SECRET_KEY'] = os.environ.get("SECRET_KEY")
 ckeditor = CKEditor(app)
 Bootstrap(app)
 login_manager = LoginManager()
@@ -20,9 +21,8 @@ login_manager.init_app(app)
 LOGIN_DISABLED = False
 
 ##CONNECT TO DB
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///blog.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("VAR_NAME")
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-app.config['SECRET_KEY'] = 'my-Super-Duper-secret-4845684565'
 db = SQLAlchemy(app)
 
 
